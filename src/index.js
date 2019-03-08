bd1bf9c1a08e448d3a23c127d202a822cb2c6830
AgnieszkaKurek <agniechakurek@gmail.com>
2019-03-08 17:31:41 +0100

    display info abaut  next player

--- src/index.js
+++ src/index.js
@@ -55,7 +55,8 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player:' +
+      (this.state.xIsNext ? "X" : "O") ;
   
       return (
         <div>
